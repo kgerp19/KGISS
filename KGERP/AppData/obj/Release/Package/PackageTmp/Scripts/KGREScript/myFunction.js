ac17f51eb1cb218fa37db07880abc5651fd5e811
Rafiqul Islam <rafiqulislamnp@hotmail.com>
2024-09-22 09:18:44 +0600

    mamun

--- KGERP/AppData/obj/Release/Package/PackageTmp/Scripts/KGREScript/myFunction.js
+++
@@ -1,8 +0,0 @@
-﻿function GetMessage(msgType, msg) {
-    var div = "<div class='alert alert-" + msgType + " alert-dismissible' role='alert'>" +
-        "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-        "<span aria-hidden='true'>&times;</span>" +
-        "</button>" + msg +
-        "</div>";
-    return div;
-}
