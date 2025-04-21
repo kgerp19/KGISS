dc266adb46a1ef13abe8b1071a3dd5c70c7fb3c6
Iyamin <iyaminrtg@gmail.com>
2025-04-21 14:09:19 +0600

    Update SalesTransferDetail data types for accuracy
    Changed `LotNumber` from `varbinary` to `nvarchar` for Unicode support. Updated `TransferQuantity` from `float` to `decimal` with precision and scale for better financial representation. These modifications are reflected in the Entity Framework model and the C# class.

--- Source/KGERP.Data/Models/SalesTransferDetail.cs
+++ Source/KGERP.Data/Models/SalesTransferDetail.cs
@@ -18,8 +18,8 @@ namespace KGERP.Data.Models
         public long SalesTransferId { get; set; }
         public long OrderDeliverDetailId { get; set; }
         public int ProductId { get; set; }
-        public byte[] LotNumber { get; set; }
-        public Nullable<double> TransferQuantity { get; set; }
+        public string LotNumber { get; set; }
+        public Nullable<decimal> TransferQuantity { get; set; }
         public string CreatedBy { get; set; }
         public System.DateTime CreateDate { get; set; }
         public string ModifiedBy { get; set; }
